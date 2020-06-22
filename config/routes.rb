eb426c83a2d572b60e20a7716f07ab7affe7e276
William Stevens <wstevens.hornet@gmail.com>
2020-06-22 20:24:05 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.orgresources :students, only: [:index, :show]
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org
   
-  get '/students/:id/activate', to: 'students#activate', as: 'activate_student'
 end
